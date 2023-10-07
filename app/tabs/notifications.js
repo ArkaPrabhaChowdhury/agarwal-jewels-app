7601c81de9a28ba17b213bae64fbe67496a3d8f6
Ruffy45 <74126164+Ruffy45@users.noreply.github.com>
2023-10-07 11:56:34 +0530

    added axios

--- app/tabs/notifications.js
+++ app/tabs/notifications.js
@@ -1,50 +1,54 @@
-import React from 'react'
-import { View, Text , Card } from 'react-native-ui-lib'
-import { Dimensions } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { View, Text, Card } from 'react-native-ui-lib'
 import axios from 'axios'
 
-axios.get('http://localhost:2000/notifications')
-  .then(response => {
-    console.log(response.data)
-    // Do something with the response data
-  })
-  .catch(error => {
-    console.error(error)
-  })
+const NotificationsScreen = () => {
+  const [data, setData] = useState(null)
 
-const NotifcationsScreen = () => {
-  const screenWidth = Math.round(Dimensions.get('window').width)
-  const cardWidth = screenWidth * 0.9
-  const cardHeight = cardWidth
+  useEffect(() => {
+    axios.get('http://localhost:2000/notifications')
+      .then(response => {
+        setData(response.data)
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }, [])
+
   return (
     <View
-    style={{
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    }}
-  >
-    <Card
       style={{
-        width: 200,
-        height: 200,
-        backgroundColor: 'white',
-        borderRadius: 10,
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
         padding: 20,
-        shadowColor: '#000',
-        shadowOffset: {
-          width: 0,
-          height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-        elevation: 5,
       }}
     >
-      <Text>Sample Text</Text>
-    </Card>
-  </View>
+      {data ? (
+        <Card
+          style={{
+            width: 200,
+            height: 200,
+            backgroundColor: 'white',
+            borderRadius: 10,
+            padding: 20,
+            shadowColor: '#000',
+            shadowOffset: {
+              width: 0,
+              height: 2,
+            },
+            shadowOpacity: 0.25,
+            shadowRadius: 3.84,
+            elevation: 5,
+          }}
+        >
+          <Text>{data}</Text>
+        </Card>
+      ) : (
+        <Text>Loading...</Text>
+      )}
+    </View>
   )
 }
 
-export default NotifcationsScreen
+export default NotificationsScreen
