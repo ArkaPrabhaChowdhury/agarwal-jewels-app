b41f7cbefe0851ca4912aa214704ac6e341dfc3c
Ruffy45 <74126164+Ruffy45@users.noreply.github.com>
2023-10-10 13:30:53 +0530

    image upload functionality

--- app/tabs/profilePage.js
+++ app/tabs/profilePage.js
@@ -1,7 +1,9 @@
-import { useState } from "react";
-import { ScrollView } from "react-native";
+import React, { useState } from "react";
+import { ScrollView, Image, Alert } from "react-native";
 import { Avatar, Button, Card, Text, View } from "react-native-ui-lib";
+import { launchImageLibrary } from 'react-native-image-picker'; // Import image picker
 import { theme } from "../styles";
+import * as ImagePicker from 'expo-image-picker';
 
 const ProfilePage = () => {
   const [user, setUser] = useState({
@@ -9,6 +11,32 @@ const ProfilePage = () => {
     email: "john.doe@example.com",
     phone: "+917981047462",
   });
+
+ 
+    const [image, setImage] = useState(null);
+    const pickImage = async () => {
+      // No permissions request is necessary for launching the image library
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
+  
+      console.log(result);
+  
+      if (!result.canceled) {
+        setImage(result.assets[0].uri);
+      }
+    };
+  
+  const uploadImage = () => {
+    // Implement image upload logic here
+    // You can send the image data to your backend for processing and storage
+    // For now, we'll just log the image URI
+    console.log('Image URI:', image);
+    Alert.alert('Image uploaded successfully!');
+  };
 
   return (
     <ScrollView flex-1 center>
@@ -25,16 +53,27 @@ const ProfilePage = () => {
 
       </Card>
 
+      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+      
       <View marginT-24 center>
         <Button
-          label="Edit Profile"
+          label="Select Image"
           backgroundColor={theme}
-          onPress={() => {
-            // Navigate to the edit profile page
-          }}
+          onPress={pickImage}
         />
+        {image && (
+          <Button
+            label="Upload Image"
+            backgroundColor={theme}
+            onPress={uploadImage}
+            marginT-12
+          />
+          
+        )}
+        
       </View>
     </ScrollView>
   );
 };
+
 export default ProfilePage;
