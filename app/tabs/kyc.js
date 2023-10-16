c8d218bdca51516a7151d136443533689466835a
Arka Prabha Chowdhury <arkopra@gmail.com>
2023-10-16 11:13:53 +0530

    lots of stuff

---
+++ app/tabs/kyc.js
@@ -0,0 +1,142 @@
+import React, { useState } from "react";
+import { Button, View } from "react-native-ui-lib";
+import { commonStyles, theme } from "../styles";
+import * as ImagePicker from "expo-image-picker";
+import { TextInput } from "react-native";
+import { Image } from "react-native";
+import { apiURL } from "../../utils";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { TouchableOpacity } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "expo-router";
+
+const kycScreen = () => {
+  //FrontEnd Image Uploading for client
+  const [image, setImage] = useState(null);
+  const [kyc_num, setKyc_num] = useState("");
+  const navigation = useNavigation();
+
+  // KYC Number Upload
+  const handleKycNumUpload = async () => {
+    const clientId = await AsyncStorage.getItem("userId");
+    const url = `${apiURL}/users/${clientId}`;
+    const data = {
+      kyc_number: kyc_num,
+    };
+    console.log(data);
+    try {
+      const response = await axios.patch(url, data);
+      console.log("Success:", response.data);
+      // Handle success response from server
+    } catch (error) {
+      console.error("Error:", error);
+      // Handle error response from server
+    }
+  };
+
+  const handleBackButtonPress = () => {
+    navigation.navigate("Profile");
+    return true;
+  };
+
+  const handleKycNum = (text) => {
+    setKyc_num(text);
+    console.log(text);
+  };
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+    console.log(result);
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
+  // Upload image to server
+  const uploadImage = async () => {
+    try {
+      if (!image) {
+        console.log("No image selected.");
+        return;
+      }
+      // Prepare the data to send in the request
+      const imageData = {
+        kyc_image: image, // Assuming image contains the base64 image data or image URI
+        // Other KYC data if needed
+      };
+      const clientId = await AsyncStorage.getItem("userId");
+
+      const apiUrl = `${apiURL}/users/${clientId}`; // Replace with the user's ID
+      const response = await axios.patch(apiUrl, imageData);
+
+      console.log("Image uploaded successfully:", response.data);
+      this.toast.show("Image uploaded successfully!");
+    } catch (error) {
+      console.error("Error uploading image:", error);
+      this.toast.show("Error uploading image. Please try again.");
+    }
+  };
+
+  return (
+    <View>
+      <TouchableOpacity
+        style={{ position: "absolute", top: 40, left: 20 }}
+        onPress={handleBackButtonPress}
+      >
+        <Ionicons name="arrow-back" size={26} color="#333" />
+      </TouchableOpacity>
+      <View marginT-84 center>
+        <Button
+          label="Select Image"
+          backgroundColor={theme}
+          onPress={pickImage}
+        />
+
+        {image && (
+          <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
+        )}
+
+        {image && (
+          <Button
+            label="Upload Image"
+            backgroundColor={theme}
+            onPress={uploadImage}
+            marginT-12
+          />
+        )}
+
+        <View marginT-10>
+          <TextInput
+            style={[
+              commonStyles.input,
+              {
+                marginLeft: 10,
+                width: 220,
+              },
+            ]}
+            marginL-20
+            placeholder="Enter your Pan/Aadhar number"
+            value={kyc_num}
+            onChangeText={handleKycNum}
+          />
+          <Button
+            marginT-10
+            backgroundColor={theme}
+            color="white"
+            onPress={handleKycNumUpload}
+            label="Submit"
+          />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default kycScreen;
--- app/tabs/notifications.js
+++ app/tabs/notifications.js
@@ -3,13 +3,21 @@ import { View, Text, Card } from "react-native-ui-lib";
 import axios from "axios";
 import { apiURL } from "../../utils";
 import { ScrollView } from "react-native";
-
+import Loading from "./loading";
+import { useFocusEffect } from "expo-router";
 
 const NotificationsScreen = () => {
   const [data, setData] = useState(null);
 
-  useEffect(() => {
-    axios.get(`${apiURL}/notifications`)
+  useFocusEffect(
+    React.useCallback(() => {
+      getNotifications();
+    }, [])
+  );
+
+  const getNotifications = async () => {
+    axios
+      .get(`${apiURL}/notifications`)
       .then((response) => {
         console.log(response.data);
         setData(response.data);
@@ -17,41 +25,47 @@ const NotificationsScreen = () => {
       .catch((error) => {
         console.log(error);
       });
-  }, []);
+  };
 
   return (
     <View
-    style={{
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-      padding: 20,
-    }}
-  >
-    <ScrollView>
-      {data ? data.map((item, index) => (
-        <Card
-          key={index}
-          style={{
-            backgroundColor: "white",
-            borderRadius: 10,
-            padding: 90,
-            shadowColor: "#000",
-            shadowOffset: {
-              width: 0,
-              height: 2,
-            },
-            shadowOpacity: 0.25,
-            shadowRadius: 3.84,
-            elevation: 5,
-            marginBottom: 20,
-          }}
-        >
-          <Text>{item.content}</Text>
-        </Card>
-      ))  : <Text>Loading...</Text>}
-    </ScrollView>
-  </View>
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 20,
+      }}
+    >
+      <ScrollView>
+        {data ? (
+          data.map((item, index) => (
+            <Card
+              key={index}
+              style={{
+                backgroundColor: "white",
+                borderRadius: 10,
+                padding: 20,
+                shadowColor: "#000",
+                shadowOffset: {
+                  width: 0,
+                  height: 2,
+                },
+                shadowOpacity: 0.25,
+                shadowRadius: 3.84,
+                elevation: 5,
+                marginBottom: 20,
+              }}
+            >
+              <Text>{item.content}</Text>
+            </Card>
+          ))
+        ) : (
+          <Text>
+            <Loading />
+          </Text>
+        )}
+      </ScrollView>
+    </View>
   );
 };
 
